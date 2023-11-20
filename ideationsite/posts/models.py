036f5dcac68509941542ec8c826d90c790382751
Salemandreus <salemandreus@gmail.com>
2023-11-20 07:03:01 +0200

    Update Post model field properties

--- ideationsite/posts/models.py
+++ ideationsite/posts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-
 # Create your models here.
 
 # class Author(models.Model):
@@ -13,7 +12,8 @@ from django.utils import timezone
 
 class Post(models.Model):
     # author = models.ForeignKey(Author, on_delete=models.CASCADE) # Todo: add a warning that deleting an account deletes all posts and recommend to export them first - make this a "delete my account, posts and all my data" option)
-    title = models.TextField()
+    title = models.CharField()
+    slug = models.SlugField(unique=True)
     content = models.TextField(null=True, blank=True)   # Todo: add markdown support (incl for title)
     pub_date = models.DateTimeField(default=timezone.now, blank=True) #(auto_now_add=True, blank=True) Todo: this seems preferred - would like it to be uneditable in admin, maybe
                                                             # Todo: add storage for user images etc?
