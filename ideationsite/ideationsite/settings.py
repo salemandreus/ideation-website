ad8bec0dbae890f68d65026ce783a35ce441dbed
Salemandreus <salemandreus@gmail.com>
2023-11-20 06:55:05 +0200

    Installed_Apps: PostsConfig dot notation  to just "posts"

--- ideationsite/ideationsite/settings.py
+++ ideationsite/ideationsite/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts.apps.PostsConfig',
+    # 'posts.apps.PostsConfig',
+    'posts',
 ]
 
 MIDDLEWARE = [
