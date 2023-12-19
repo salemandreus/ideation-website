c0891a2e873a591a3af6eb6f4a88e39335c805fa
Salemandreus <salemandreus@gmail.com>
2023-12-19 17:15:40 +0200

    Add pagination to search, search uses list.html too

--- src/ideationsite/searches/views.py
+++ src/ideationsite/searches/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from posts.models import Post
 
@@ -10,8 +11,23 @@ def search_view(request):
     if request.user.is_authenticated:
         user=request.user
     context = {"query": query}
+
+    posts_and_threads_counts = []
     if query is not None:
         SearchQuery.objects.create(user=user, query=query)
         posts_list = Post.objects.search(query=query)
-        context['posts_list'] = posts_list
+
+        # Add to new list with threads (children) counts of each
+        for post_object in posts_list:
+            posts_and_threads_counts.append([post_object, post_object.responses.count()])
+
+        # Add Pagination
+        paginator = Paginator(posts_and_threads_counts, 15)
+        page_number = request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+
+        context['page_obj'] = page_obj
+        context['results_count'] = posts_and_threads_counts.__len__()
+
+
     return render(request, 'searches/view.html', context)
