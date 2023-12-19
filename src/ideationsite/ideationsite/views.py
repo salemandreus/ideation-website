8c5f3fabbaf3c133ec2e9fc1c2eeea43a6ad208d
Salemandreus <salemandreus@gmail.com>
2023-12-19 14:59:28 +0200

    paginator page indication only shows if necessary

--- src/ideationsite/ideationsite/views.py
+++ src/ideationsite/ideationsite/views.py
@@ -27,7 +27,6 @@ def index(request):
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
-
     context["page_obj"] = page_obj
 
     context["utc_now" ] = datetime.now(timezone.utc)
