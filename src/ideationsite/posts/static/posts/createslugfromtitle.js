245521ee253216e2bf889a8d805ddd329dc46be8
Salemandreus <salemandreus@gmail.com>
2023-12-29 07:19:33 +0200

    Slugify form js trims non-word chars that would become single dashes at start/end

--- src/ideationsite/posts/static/posts/createslugfromtitle.js
+++ src/ideationsite/posts/static/posts/createslugfromtitle.js
@@ -5,6 +5,8 @@ const slugify = (val) => {
     return val.toString().toLowerCase().trim()
         .replace(/&/g, '-and-') // replacing & with '-and-'
         .replace(/[\s\W-]+/g, '-') //replcaing spaces, non-word chars and dashes with a single '-'
+        .replace(/^\W+/g,'') //remove start char if not a word char
+        .replace(/\W+$/g,'') //remove end char if not a word char
 };
 
 titleInput.addEventListener('keyup', (e) => {
